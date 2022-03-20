3530378fb75a1352a12e4d5d3c44c8fa5e5013de
shaoqufeng <shaoqufeng@kayak.com>
2022-03-20 17:22:39 +0800

    提交

--- src/main/java/com/shao/git/GitTest.java
+++ src/main/java/com/shao/git/GitTest.java
@@ -7,5 +7,7 @@ package com.shao.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+
     }
 }
