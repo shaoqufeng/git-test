1342c4ae9bae4627c833edc115a5b15ec46931ff
shaoqufeng <shaoqufeng@kayak.com>
2022-03-20 17:19:27 +0800

    提交

---
+++ src/main/java/com/shao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.shao.git;
+
+/**
+ * @author shaoqf
+ * @create 2022-03-20 17:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
